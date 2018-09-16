95ddb95ca24a58eb05002696cd893a344aed07fd
Luis Suarez <suarezluis@gmail.com>
2018-09-15 20:55:39 -0500

    added CORS

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const cheerio = require("cheerio");
 
 var name;
 const PORT = process.env.PORT || 3003;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/:beerName", (req, res) => {
   console.log("responding");
   console.log("params", req.params.beerName);
